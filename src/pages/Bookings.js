89cc5755f63311a9384f47f991016c7e520b9628
Thornhill420 <thornhill_mt@hotmail.co.uk>
2025-02-21 11:32:04 +0200

    Updated API URLs

--- src/pages/Bookings.js
+++ src/pages/Bookings.js
@@ -13,6 +13,12 @@ const Bookings = () => {
         notes: "",
     });
 
+    const timeSlots = [
+        "08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM",
+        "12:00 PM", "01:00 PM", "02:00 PM", "03:00 PM",
+        "04:00 PM", "05:00 PM"
+    ];
+
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
@@ -65,7 +71,12 @@ const Bookings = () => {
                 <input type="date" name="date" value={formData.date} onChange={handleChange} required />
 
                 <label>Select a Time:</label>
-                <input type="time" name="time" value={formData.time} onChange={handleChange} required />
+                <select name="time" value={formData.time} onChange={handleChange} required>
+                    <option value="">Select a time</option>
+                    {timeSlots.map((slot) => (
+                        <option key={slot} value={slot}>{slot}</option>
+                    ))}
+                </select>
 
                 <label>Your Name:</label>
                 <input type="text" name="name" value={formData.name} onChange={handleChange} required />
