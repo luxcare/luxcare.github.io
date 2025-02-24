ffad63304f785bf6a883f151be69343ac55cbeaf
Thornhill420 <thornhill_mt@hotmail.co.uk>
2025-02-24 14:56:27 +0200

    Deploy website to luxcare.github.io

--- update git.bat
+++ update git.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "C:\Users\Mike\Music\LuxCare Cleaning Website\Luxcare_Cleaning_Website\frontend\luxcare_cleaning_website"
 git add .
-git commit -m "Updated API URLs"
+git commit -m "Deploy website to luxcare.github.io"
 git push origin main
 npm run deploy
 pause
