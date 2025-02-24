b9fbb27a8169e3fa045b3b718962da7e640cfd77
Thornhill420 <thornhill_mt@hotmail.co.uk>
2025-02-24 12:40:34 +0200

    Updated API URLs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
-import Bookings from "./pages/Bookings"; // Import the Bookings page
+import Bookings from "./pages/Bookings";
 import Contact from "./pages/Contact";
-import Header from "./components/Header"; // Make sure Header is imported only here
+import Header from "./components/Header"; 
 import "./App.css";
 
 function App() {
     return (
         <Router>
-            <Header /> {/* This should be the only Header */}
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/services" element={<Services />} />
-                <Route path="/bookings" element={<Bookings />} /> {/* New Bookings Page */}
+                <Route path="/bookings" element={<Bookings />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
         </Router>
